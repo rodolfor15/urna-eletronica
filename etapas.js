eb258ad243d8586432b45bae78f926d499c059cc
Nome do Usuário <email_do.usuario@servidor.com>
2022-07-12 16:52:42 -0300

    simulador de urn eletronica

---
+++ etapas.js
@@ -0,0 +1,185 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO FEDERAL',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '9999',
+                nome: 'Edilson',
+                partido: 'ATA',
+                fotos:[
+                    {url:'9999.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '9988',
+                nome: 'Ricardinho',
+                partido: 'ATA',
+                fotos:[
+                    {url:'9988.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '9111',
+                nome: 'Roberto Carlos',
+                partido: 'DEF',
+                fotos:[
+                    {url:'9111.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '9122',
+                nome: 'Lucio',
+                partido: 'DEF',
+                fotos:[
+                    {url:'9122.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+            {
+                numero: '9133',
+                nome: 'Vampeta',
+                partido: 'DEF',
+                fotos:[
+                    {url:'9133.jpg', legenda: 'Deputado Federal'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '99999',
+                nome: 'Juninho Paulista',
+                partido: 'ATA',
+                fotos:[
+                    {url:'99999.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '99888',
+                nome: 'Kleberson',
+                partido: 'ATA',
+                fotos:[
+                    {url:'99888.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '91111',
+                nome: 'Anderson Polga',
+                partido: 'DEF',
+                fotos:[
+                    {url:'91111.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '91222',
+                nome: 'Edimilson',
+                partido: 'DEF',
+                fotos:[
+                    {url:'91222.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+            {
+                numero: '91333',
+                nome: 'Belleti',
+                partido: 'DEF',
+                fotos:[
+                    {url:'91333.jpg', legenda: 'Deputado Estadual'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'SENADOR',
+        numeros: 3,
+        candidatos: [
+            {
+                numero: '999',
+                nome: 'Rivaldo',
+                partido: 'ATA',
+                fotos:[
+                    {url:'999.jpg', legenda: 'Senador'}
+                ]
+            },
+            {
+                numero: '998',
+                nome: 'Kaka',
+                partido: 'ATA',
+                fotos:[
+                    {url:'998.jpg', legenda: 'Senador'}
+                ]
+            },
+            {
+                numero: '911',
+                nome: 'Dida',
+                partido: 'DEF',
+                fotos:[
+                    {url:'911.jpg', legenda: 'Senador'}
+                ]
+            },
+            {
+                numero: '912',
+                nome: 'Roque Junior',
+                partido: 'DEF',
+                fotos:[
+                    {url:'912.jpg', legenda: 'Senador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'Governador',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ronaldinho Gaúcho',
+                partido: 'ATA',
+                vice: 'Denilson',
+                fotos:[
+                    {url:'99_3.jpg', legenda: 'Governador'},
+                    {url:'99_4.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+            {
+                numero: '91',
+                nome: 'Cafu',
+                partido: 'DEF',
+                vice: 'Junior',
+                fotos:[
+                    {url:'91_3.jpg', legenda: 'Governador'},
+                    {url:'91_4.jpg', legenda: 'Vice-Governador', small: true}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ronaldo',
+                partido: 'ATA',
+                vice: 'Luizão',
+                fotos:[
+                    {url:'99.jpg', legenda: 'Presidente'},
+                    {url:'99_2.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+            {
+                numero: '91',
+                nome: 'Marcos',
+                partido: 'DEF',
+                vice: 'Rogerio Ceni',
+                fotos:[
+                    {url:'91.jpg', legenda: 'Presidente'},
+                    {url:'91_2.jpg', legenda: 'Vice-Presidente', small: true}
+                ]
+            },
+        ]
+    }
+];
